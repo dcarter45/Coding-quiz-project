b1e2d780dcd7575b5dea868a9149b97100649ed1
Dacrter45 <Darrellcarter22@gmail.com>
2021-07-20 22:46:49 -0400

    initial commit Psuedo code

--- README.md
+++
@@ -1,1 +0,0 @@
-# Coding-quiz-project
---
+++ assets/script.js
@@ -0,0 +1,28 @@
+
+button.addEventListener('click', function(event)){
+    function displayQuestion & start timer 
+}
+
+button.addEventListener('click', function(event)){
+    userAnswer=  the answer that they clicked
+    set interval- continue to check for time until time equals 0.. web api 09
+
+    if(userAnswer=== right answer){
+        display 'correct'
+       function displayQuestion
+    }
+    else{
+        display 'incorrect'
+        timer--;
+        if(set interval timer>0 || quesions left=0){
+            end game
+        }
+        function displayQuestion 
+    }
+
+}
+
+
+else{
+    function displayQuestion
+}
